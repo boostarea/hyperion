510bb41bd5ff3f54e4241c8bd890f236e9fba900
chenrong <18649808198@163.com>
2018-08-24 17:04:59 +0800

    add fair lock

---
+++ src/main/java/com/company/FairLock/FairLock.java
@@ -0,0 +1,29 @@
+package com.company.FairLock;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * 公平锁实现成本高,默认情况,使用非公平锁
+ *
+ */
+public class FairLock implements Runnable {
+    static ReentrantLock fairLock = new ReentrantLock(true);
+
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                fairLock.lock();
+                System.out.println(Thread.currentThread() + "Get");
+            } finally {
+                fairLock.unlock();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        FairLock r1 = new FairLock();
+        new Thread(r1, "r1").start();
+        new Thread(r1, "r2").start();
+    }
+}
