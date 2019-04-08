324a865217fd8aab5ab1bf33fc62fd63f81a274c
rong.chen <18649808198@163.com>
2019-04-08 15:27:01 +0800

    add Exchanger demo

---
+++ src/main/java/com/ooooor/basic/exchanger/ExchangerDemo.java
@@ -0,0 +1,42 @@
+package com.ooooor.basic.exchanger;
+
+import java.util.concurrent.Exchanger;
+
+/**
+ * @Description 造成阻塞，互相等待
+ * @Author ooooor
+ * @Date 2019-04-08 15:19
+ **/
+public class ExchangerDemo extends Thread {
+
+    private Exchanger<String> exchanger;
+    private String string;
+    private String threadName;
+
+    public ExchangerDemo(Exchanger<String> exchanger, String string,
+                         String threadName) {
+        this.exchanger = exchanger;
+        this.string = string;
+        this.threadName = threadName;
+    }
+
+    public void run() {
+        try {
+            System.out.println(threadName + ": " + exchanger.exchange(string));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Exchanger<String> exchanger = new Exchanger<>();
+        ExchangerDemo test1 = new ExchangerDemo(exchanger, "string1",
+                "thread-1");
+        ExchangerDemo test2 = new ExchangerDemo(exchanger, "string2",
+                "thread-2");
+
+        test1.start();
+        Thread.sleep(2000L);
+        test2.start();
+    }
+}
